4e80f94ce2c632379a9a0ec4fe9824dbd04d465e
gowthamirdd <gowthamirdd@gmail.com>
2015-04-01 22:09:55 -0700

    Project Final Version

---
+++ src/com/comcast/interview/taxperson/CalculateTax.java
@@ -0,0 +1,26 @@
+package com.comcast.interview.taxperson;
+
+/**
+ * Created by Gowthami on 4/1/15.
+ */
+public class CalculateTax {
+
+
+    //Assumption is price is always in pennies and question says single item.
+    public  Long calculate(int price, boolean isLuxury) throws RuntimeException {
+        if(price <= 0){
+            throw new RuntimeException("Price Cannot be '0' or less than '0'. The value user entered is : " + price);
+        }
+        long finalPrice;
+            if(!isLuxury){
+                finalPrice = (long) Math.abs((price*0.01)+price);
+            } else{
+                finalPrice = (long) Math.abs((price*0.09)+price);
+            }
+
+        return finalPrice;
+    }
+
+
+
+}
