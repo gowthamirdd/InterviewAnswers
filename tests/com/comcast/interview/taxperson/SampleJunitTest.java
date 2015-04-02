028720e6171405124e65c46bf8bd6584e9a90b88
gowthamirdd <gowthamirdd@gmail.com>
2015-04-01 22:11:49 -0700

    Project Final Version

---
+++ tests/com/comcast/interview/taxperson/SampleJunitTest.java
@@ -0,0 +1,36 @@
+package com.comcast.interview.taxperson;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class SampleJunitTest {
+
+    public CalculateTax calculateTax;
+
+    @Before
+    public void setup() throws Exception{
+        calculateTax = new CalculateTax();
+    }
+
+    @Test
+    public void necessaryItemTest() throws RuntimeException {
+        System.out.println(calculateTax.calculate(100,false));
+    }
+
+    @Test
+    public void luxuryItemTest() throws RuntimeException {
+        System.out.println(calculateTax.calculate(100,true));
+    }
+    
+    @Test
+    public void zeroTest() throws RuntimeException{
+        System.out.println(calculateTax.calculate(0,true));
+    }
+    
+    @Test
+    public void lessThanZeroTest() throws RuntimeException{
+        System.out.println(calculateTax.calculate(-1000,true));
+    }
+
+}
